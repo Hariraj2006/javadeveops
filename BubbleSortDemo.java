b515737aa3f9b5123c7d70aa535caeaa240fceb0
Hariraj2006 <harisgsmediasoft@gmail.com>
2026-03-09 09:48:55 +0530

    Create BubbleSortDemo.java

---
+++ BubbleSortDemo.java
@@ -0,0 +1,33 @@
+class BubbleSortDemo {
+
+    public static void main(String args[]) {
+
+        int arr[] = {5, 3, 8, 4, 2};
+
+        System.out.println("Array Before Sorting:");
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+
+                if(arr[j] > arr[j+1]){
+
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+
+                }
+
+            }
+        }
+
+        System.out.println("\nArray After Bubble Sort:");
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
